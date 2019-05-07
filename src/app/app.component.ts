38973156107693b6d56eab4d39af844dfd138181
Hela <helalad@gmail.com>
2019-05-07 16:35:59 +0200

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fireBaseAngular';
+
+  constructor() {
+
+    const config = {
+
+      apiKey: 'AIzaSyB1e6x5wGZjVTIhe86L3KyNhCxFoJebIHg',
+
+      authDomain: 'my-first-project-4024f.firebaseapp.com',
+
+      databaseURL: 'https://my-first-project-4024f.firebaseio.com',
+
+      projectId: 'my-first-project-4024f',
+
+      storageBucket: 'my-first-project-4024f.appspot.com',
+
+      messagingSenderId: '797791080048'
+
+    };
+
+    firebase.initializeApp(config);
+
+  }
 }
